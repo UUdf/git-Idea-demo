07c43b93704de883721aae96beba05ca939dc1e2
UUdf <87841683+UUdf@users.noreply.github.com>
2022-04-27 08:50:20 +0800

    Update git_demo.java

--- src/main/java/com/swjd/git_demo.java
+++ src/main/java/com/swjd/git_demo.java
@@ -4,5 +4,6 @@ public class git_demo {
     public static void main(String[] args) {
         System.out.println("git hello");
         System.out.println("git push test");
+        System.out.println("git pull test");
     }
 }
