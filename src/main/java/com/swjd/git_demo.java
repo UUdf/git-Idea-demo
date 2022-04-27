003bae98c1741526da094d367e00d1f056369180
Joe <Joe@qq.com>
2022-04-27 08:41:09 +0800

    git push test

--- src/main/java/com/swjd/git_demo.java
+++ src/main/java/com/swjd/git_demo.java
@@ -3,5 +3,6 @@ package com.swjd;
 public class git_demo {
     public static void main(String[] args) {
         System.out.println("git hello");
+        System.out.println("git push test");
     }
 }
